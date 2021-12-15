30478a3f81f6cf0a04657f8067a5853e3f7caad5
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-12-15 13:05:32 +0000

    Changing port number

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ server.use(middlewares)
 server.use(jsonServer.bodyParser)
 
 server.use(router)
-server.listen(8080, () => {
+server.listen(3000, () => {
   console.log('JSON Server is running')
 })
