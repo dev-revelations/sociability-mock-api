aa355c3df152385a277a6cfddff197dee5b24236
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-12-15 18:20:38 +0000

    supporting cors

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const argv = require('yargs').argv;
-const path = require('path')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
 const port = argv.port || 8081;
 
+server.use(cors());
+
 server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1'
-  })
-)
+    jsonServer.rewriter({
+        '/api/*': '/$1'
+    })
+);
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
-server.use(router)
+server.use(router);
 server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
